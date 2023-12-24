43d1473713ea2259bc269e34c2d204fed0215029
ShivaShankar <ss3177806@gmail.com>
2023-12-24 15:02:21 +0530

    STYLES UPDATED

--- src/Components/Signup/SignUp.jsx
+++ src/Components/Signup/SignUp.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from "react";
-import {  Link as RouterLink } from 'react-router-dom';
-import './SignUp.css'; 
+import { Link as RouterLink } from 'react-router-dom';
+import './SignUp.css';
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -16,9 +16,8 @@ const SignUp = () => {
 
   const [emailOtpMessage, setEmailOtpMessage] = useState("");
   const [mobileOtpMessage, setMobileOtpMessage] = useState("");
- 
 
-  const emailInputRef = useRef(null);
+  const nameInputRef = useRef(null);
 
   // Validation Part
   const isEmailValid = /[a-z]+@gmail.com/.test(email);
@@ -42,31 +41,27 @@ const SignUp = () => {
     }
   }, [isMobileOtpSent]);
 
-  
-
   useEffect(() => {
     setIsFormSubmitted(false);
   }, [email, emailOtp, mobile, mobileOtp, name, password]);
 
   useEffect(() => {
-    emailInputRef.current.focus();
+    nameInputRef.current.focus();
   }, []);
 
-  const sendEmailOtp = () => {
-    // Simulate OTP sending logic for email
+  const sendEmailOtp = (e) => {
+    e.preventDefault();
     setTimeout(() => {
       setIsEmailOtpSent(true);
     }, 1000);
   };
 
-  const sendMobileOtp = () => {
-    // Simulate OTP sending logic for mobile
+  const sendMobileOtp = (e) => {
+    e.preventDefault();
     setTimeout(() => {
       setIsMobileOtpSent(true);
     }, 1000);
   };
-
- 
 
   const isFormValid =
     isEmailValid &&
@@ -76,149 +71,132 @@ const SignUp = () => {
     isNameValid &&
     isPasswordValid;
 
-  return (
-    <div className="registration-form-container">
-      {!isFormSubmitted && (
-        <form >
-       <center>
-       <h2 style={{marginTop: '40px', fontWeight: '400'}}>
-        Get Started Now
-        </h2>
-        <p>
-        It's free to join and gain full access to thousands of exciting investment opportunities.
-        </p>
-       </center>
-          <label>Name:</label>
-          <div
-            className={`input-container ${isNameValid ? "valid" : "invalid"}`}
-          >
-            <input
-              type="text"
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              placeholder="Enter your name"
-            />
-            {isNameValid && <div className="validation-icon">&#10004;</div>}
-          </div>
-
-          <label>Email</label>
-          <div
-            className={`input-container ${isEmailValid ? "valid" : "invalid"}`}
-          >
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              disabled={isEmailOtpSent}
-              placeholder="example@gmail.com"
-              ref={emailInputRef}
-            />
-            {isEmailValid && <div className="validation-icon">&#10004;</div>}
-          </div>
-
-          {emailOtpMessage && (
-            <div className="success-message">{emailOtpMessage}</div>
-          )}
-          {!isEmailOtpSent && (
-            <button onClick={sendEmailOtp} disabled={!isEmailValid}>
-              Send Email OTP
+    return (
+      <div className="registration-form-container">
+        {!isFormSubmitted && (
+          <form>
+            <center>
+              <h2 style={{ marginTop: '40px', fontWeight: '400' }}>
+                Get Started Now
+              </h2>
+              <p>
+                It's free to join and gain full access to thousands of exciting investment opportunities.
+              </p>
+            </center>
+            <div
+              className={`input-container ${isNameValid ? "valid" : "invalid"}`}
+            >
+              <input
+                type="text"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                placeholder="Full Name*"
+                ref={nameInputRef}
+              />
+            </div>
+            
+            <div
+              className={`input-container ${isEmailValid ? "valid" : "invalid"}`}
+            >
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                disabled={isEmailOtpSent}
+                placeholder="Email Address*"
+              />
+               {emailOtpMessage && (
+              <div className="success-message">{emailOtpMessage}</div>
+            )}
+            {!isEmailOtpSent && (
+              <button type="button" onClick={sendEmailOtp} disabled={!isEmailValid}>
+                Get OTP
+              </button>
+            )}
+            </div>
+    
+           
+    
+            {isEmailOtpSent && (
+              <>
+                <div
+                  className={`input-container ${
+                    isEmailOtpValid ? "valid" : "invalid"
+                  }`}
+                >
+                  <input
+                    type="text"
+                    value={emailOtp}
+                    onChange={(e) => setEmailOtp(e.target.value)}
+                    placeholder="Enter Email OTP"
+                  />
+                </div>
+              </>
+            )}
+            
+            <div
+              className={`input-container ${
+                isMobileValid ? "valid" : "invalid"
+              }`}
+            >
+              <input
+                type="text"
+                value={mobile}
+                onChange={(e) => setMobile(e.target.value)}
+                placeholder="Mobile Number*"
+              />
+              {mobileOtpMessage && (
+              <div className="success-message">{mobileOtpMessage}</div>
+            )}
+            {!isMobileOtpSent && (
+              <button type="button" onClick={sendMobileOtp} disabled={!isMobileValid}>
+                Get OTP
+              </button>
+            )}
+            </div>
+    
+            
+    
+            {isMobileOtpSent && (
+              <>
+                <div
+                  className={`input-container ${
+                    isMobileOtpValid ? "valid" : "invalid"
+                  }`}
+                >
+                  <input
+                    type="text"
+                    value={mobileOtp}
+                    onChange={(e) => setMobileOtp(e.target.value)}
+                    placeholder="Enter Mobile OTP"
+                  />
+                </div>
+              </>
+            )}
+           
+            <div
+              className={`input-container ${
+                isPasswordValid ? "valid" : "invalid"
+              }`}
+            >
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                placeholder="Create Password"
+              />
+            </div>
+    
+            <button className="submit-button" type="submit" disabled={!isFormValid}>
+              Submit
             </button>
-          )}
-
-          {isEmailOtpSent && (
-            <>
-              <label>Email OTP:</label>
-              <div
-                className={`input-container ${
-                  isEmailOtpValid ? "valid" : "invalid"
-                }`}
-              >
-                <input
-                  type="text"
-                  value={emailOtp}
-                  onChange={(e) => setEmailOtp(e.target.value)}
-                  placeholder="Enter Email OTP"
-                />
-                {isEmailOtpValid && (
-                  <div className="validation-icon">&#10004;</div>
-                )}
-              </div>
-            </>
-          )}
-
-          <label>Mobile Number:</label>
-          <div
-            className={`input-container ${
-              isMobileValid ? "valid" : "invalid"
-            }`}
-          >
-            <input
-              type="text"
-              value={mobile}
-              onChange={(e) => setMobile(e.target.value)}
-              placeholder="Enter your mobile number"
-            />
-            {isMobileValid && <div className="validation-icon">&#10004;</div>}
-          </div>
-
-          {mobileOtpMessage && (
-            <div className="success-message">{mobileOtpMessage}</div>
-          )}
-          {!isMobileOtpSent && (
-            <button onClick={sendMobileOtp} disabled={!isMobileValid}>
-              Send Mobile OTP
-            </button>
-          )}
-
-          {isMobileOtpSent && (
-            <>
-              <label>Mobile OTP:</label>
-              <div
-                className={`input-container ${
-                  isMobileOtpValid ? "valid" : "invalid"
-                }`}
-              >
-                <input
-                  type="text"
-                  value={mobileOtp}
-                  onChange={(e) => setMobileOtp(e.target.value)}
-                  placeholder="Enter Mobile OTP"
-                />
-                {isMobileOtpValid && (
-                  <div className="validation-icon">&#10004;</div>
-                )}
-              </div>
-            </>
-          )}
-
-          <label>Password:</label>
-          <div
-            className={`input-container ${
-              isPasswordValid ? "valid" : "invalid"
-            }`}
-          >
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              placeholder="Enter your password"
-            />
-            {isPasswordValid && <div className="validation-icon">&#10004;</div>}
-          </div>
-
-          <button type="submit" disabled={!isFormValid}>
-            Submit
-          </button>
-          <p>
-          Already have an account? <RouterLink to="/login">Login</RouterLink>
-        </p>
-        </form>
-      )}
-      
-    </div>
+            <p>
+              Already have an account? <RouterLink to="/login">Login</RouterLink>
+            </p>
+          </form>
+        )}
+      </div>
   );
 };
 
-export default SignUp;
-
-
+export default SignUp
