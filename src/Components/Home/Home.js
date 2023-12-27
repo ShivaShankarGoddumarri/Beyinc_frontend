7af511ca48ef3fbe36f88a159991b937fb29a7f7
ShivaShankar <ss3177806@gmail.com>
2023-12-27 13:27:12 +0530

    navbar added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,10 +1,17 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <h1>
-        HOME
-    </h1>
+    <div>
+    <navbar>
+        User Name
+    <img src='profile.jpeg' alt=''/> 
+    </navbar>
+    <center>
+    <h1>Welcome User !</h1>
+    </center>
+    </div>
   )
 }
 
